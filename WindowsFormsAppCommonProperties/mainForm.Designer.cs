9ed73767887f1a192a6290068f7c80e34a8ca053
unknown <gabrieljimenez@paucasesnovescifp.cat>
2020-11-04 16:37:12 +0100

    El groupbox con todos los componentes dentro

--- WindowsFormsAppCommonProperties/mainForm.Designer.cs
+++ WindowsFormsAppCommonProperties/mainForm.Designer.cs
@@ -31,6 +31,16 @@
             this.targetButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.formSizeLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonVisible = new System.Windows.Forms.Button();
+            this.buttonEnable = new System.Windows.Forms.Button();
+            this.buttonCenter = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttondown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // targetButton
@@ -44,6 +54,15 @@
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonLeft);
+            this.groupBox1.Controls.Add(this.buttonRight);
+            this.groupBox1.Controls.Add(this.buttondown);
+            this.groupBox1.Controls.Add(this.buttonUp);
+            this.groupBox1.Controls.Add(this.buttonCenter);
+            this.groupBox1.Controls.Add(this.buttonEnable);
+            this.groupBox1.Controls.Add(this.buttonVisible);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Location = new System.Drawing.Point(570, 15);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 500);
@@ -61,6 +80,86 @@
             this.formSizeLabel.TabIndex = 2;
             this.formSizeLabel.Text = "Form size: 800, 600";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FormBorderStyle";
+            // 
+            // buttonVisible
+            // 
+            this.buttonVisible.Location = new System.Drawing.Point(14, 160);
+            this.buttonVisible.Name = "buttonVisible";
+            this.buttonVisible.Size = new System.Drawing.Size(172, 30);
+            this.buttonVisible.TabIndex = 2;
+            this.buttonVisible.Text = "Visible = true";
+            this.buttonVisible.UseVisualStyleBackColor = true;
+            // 
+            // buttonEnable
+            // 
+            this.buttonEnable.Location = new System.Drawing.Point(14, 213);
+            this.buttonEnable.Name = "buttonEnable";
+            this.buttonEnable.Size = new System.Drawing.Size(172, 30);
+            this.buttonEnable.TabIndex = 3;
+            this.buttonEnable.Text = "Enabled = true";
+            this.buttonEnable.UseVisualStyleBackColor = true;
+            // 
+            // buttonCenter
+            // 
+            this.buttonCenter.Location = new System.Drawing.Point(14, 267);
+            this.buttonCenter.Name = "buttonCenter";
+            this.buttonCenter.Size = new System.Drawing.Size(172, 30);
+            this.buttonCenter.TabIndex = 4;
+            this.buttonCenter.Text = "Center Target";
+            this.buttonCenter.UseVisualStyleBackColor = true;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(67, 341);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(50, 50);
+            this.buttonUp.TabIndex = 5;
+            this.buttonUp.Text = "^";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            // 
+            // buttondown
+            // 
+            this.buttondown.Location = new System.Drawing.Point(67, 407);
+            this.buttondown.Name = "buttondown";
+            this.buttondown.Size = new System.Drawing.Size(50, 50);
+            this.buttondown.TabIndex = 6;
+            this.buttondown.Text = "v";
+            this.buttondown.UseVisualStyleBackColor = true;
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(123, 394);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(50, 50);
+            this.buttonRight.TabIndex = 7;
+            this.buttonRight.Text = ">";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(11, 394);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(50, 50);
+            this.buttonLeft.TabIndex = 8;
+            this.buttonLeft.Text = "<";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -71,6 +170,8 @@
             this.Controls.Add(this.targetButton);
             this.Name = "mainForm";
             this.Text = "(0,0)  Winforms Components - Common Controls";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +182,15 @@
         private System.Windows.Forms.Button targetButton;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label formSizeLabel;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttondown;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonCenter;
+        private System.Windows.Forms.Button buttonEnable;
+        private System.Windows.Forms.Button buttonVisible;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
