86541bc520d9dd900f7188fdd1a6eb552b69f892
unknown <gabrieljimenez@paucasesnovescifp.cat>
2020-11-04 19:24:27 +0100

    Hechos todos los putnos pedidos, lo unico distinto es que inizialido la altura y lo ancho en la seccion de codigo el resto funciona correctamente

--- WindowsFormsAppCommonProperties/mainForm.Designer.cs
+++ WindowsFormsAppCommonProperties/mainForm.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.targetButton = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.formSizeLabel = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.buttonVisible = new System.Windows.Forms.Button();
-            this.buttonEnable = new System.Windows.Forms.Button();
-            this.buttonCenter = new System.Windows.Forms.Button();
-            this.buttonUp = new System.Windows.Forms.Button();
-            this.buttondown = new System.Windows.Forms.Button();
-            this.buttonRight = new System.Windows.Forms.Button();
             this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonCenter = new System.Windows.Forms.Button();
+            this.buttonEnable = new System.Windows.Forms.Button();
+            this.buttonVisible = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.formSizeLabel = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,7 +56,7 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.buttonLeft);
             this.groupBox1.Controls.Add(this.buttonRight);
-            this.groupBox1.Controls.Add(this.buttondown);
+            this.groupBox1.Controls.Add(this.buttonDown);
             this.groupBox1.Controls.Add(this.buttonUp);
             this.groupBox1.Controls.Add(this.buttonCenter);
             this.groupBox1.Controls.Add(this.buttonEnable);
@@ -70,23 +70,75 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Controls";
             // 
-            // formSizeLabel
+            // buttonLeft
             // 
-            this.formSizeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.formSizeLabel.AutoSize = true;
-            this.formSizeLabel.Location = new System.Drawing.Point(20, 530);
-            this.formSizeLabel.Name = "formSizeLabel";
-            this.formSizeLabel.Size = new System.Drawing.Size(148, 20);
-            this.formSizeLabel.TabIndex = 2;
-            this.formSizeLabel.Text = "Form size: 800, 600";
+            this.buttonLeft.Location = new System.Drawing.Point(11, 394);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(50, 50);
+            this.buttonLeft.TabIndex = 8;
+            this.buttonLeft.Text = "<";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
             // 
-            // comboBox1
+            // buttonRight
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(14, 71);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(172, 28);
-            this.comboBox1.TabIndex = 0;
+            this.buttonRight.Location = new System.Drawing.Point(123, 394);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(50, 50);
+            this.buttonRight.TabIndex = 7;
+            this.buttonRight.Text = ">";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(67, 407);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(50, 50);
+            this.buttonDown.TabIndex = 6;
+            this.buttonDown.Text = "v";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(67, 341);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(50, 50);
+            this.buttonUp.TabIndex = 5;
+            this.buttonUp.Text = "^";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonCenter
+            // 
+            this.buttonCenter.Location = new System.Drawing.Point(14, 267);
+            this.buttonCenter.Name = "buttonCenter";
+            this.buttonCenter.Size = new System.Drawing.Size(172, 36);
+            this.buttonCenter.TabIndex = 4;
+            this.buttonCenter.Text = "Center Target";
+            this.buttonCenter.UseVisualStyleBackColor = true;
+            this.buttonCenter.Click += new System.EventHandler(this.buttonCenter_Click);
+            // 
+            // buttonEnable
+            // 
+            this.buttonEnable.Location = new System.Drawing.Point(14, 213);
+            this.buttonEnable.Name = "buttonEnable";
+            this.buttonEnable.Size = new System.Drawing.Size(172, 35);
+            this.buttonEnable.TabIndex = 3;
+            this.buttonEnable.Text = "Enabled = true";
+            this.buttonEnable.UseVisualStyleBackColor = true;
+            this.buttonEnable.Click += new System.EventHandler(this.buttonEnable_Click);
+            // 
+            // buttonVisible
+            // 
+            this.buttonVisible.Location = new System.Drawing.Point(14, 160);
+            this.buttonVisible.Name = "buttonVisible";
+            this.buttonVisible.Size = new System.Drawing.Size(172, 36);
+            this.buttonVisible.TabIndex = 2;
+            this.buttonVisible.Text = "Visible = true";
+            this.buttonVisible.UseVisualStyleBackColor = true;
+            this.buttonVisible.Click += new System.EventHandler(this.buttonVisible_Click);
             // 
             // label1
             // 
@@ -97,68 +149,24 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "FormBorderStyle";
             // 
-            // buttonVisible
+            // comboBox1
             // 
-            this.buttonVisible.Location = new System.Drawing.Point(14, 160);
-            this.buttonVisible.Name = "buttonVisible";
-            this.buttonVisible.Size = new System.Drawing.Size(172, 30);
-            this.buttonVisible.TabIndex = 2;
-            this.buttonVisible.Text = "Visible = true";
-            this.buttonVisible.UseVisualStyleBackColor = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(172, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // buttonEnable
+            // formSizeLabel
             // 
-            this.buttonEnable.Location = new System.Drawing.Point(14, 213);
-            this.buttonEnable.Name = "buttonEnable";
-            this.buttonEnable.Size = new System.Drawing.Size(172, 30);
-            this.buttonEnable.TabIndex = 3;
-            this.buttonEnable.Text = "Enabled = true";
-            this.buttonEnable.UseVisualStyleBackColor = true;
-            // 
-            // buttonCenter
-            // 
-            this.buttonCenter.Location = new System.Drawing.Point(14, 267);
-            this.buttonCenter.Name = "buttonCenter";
-            this.buttonCenter.Size = new System.Drawing.Size(172, 30);
-            this.buttonCenter.TabIndex = 4;
-            this.buttonCenter.Text = "Center Target";
-            this.buttonCenter.UseVisualStyleBackColor = true;
-            // 
-            // buttonUp
-            // 
-            this.buttonUp.Location = new System.Drawing.Point(67, 341);
-            this.buttonUp.Name = "buttonUp";
-            this.buttonUp.Size = new System.Drawing.Size(50, 50);
-            this.buttonUp.TabIndex = 5;
-            this.buttonUp.Text = "^";
-            this.buttonUp.UseVisualStyleBackColor = true;
-            // 
-            // buttondown
-            // 
-            this.buttondown.Location = new System.Drawing.Point(67, 407);
-            this.buttondown.Name = "buttondown";
-            this.buttondown.Size = new System.Drawing.Size(50, 50);
-            this.buttondown.TabIndex = 6;
-            this.buttondown.Text = "v";
-            this.buttondown.UseVisualStyleBackColor = true;
-            // 
-            // buttonRight
-            // 
-            this.buttonRight.Location = new System.Drawing.Point(123, 394);
-            this.buttonRight.Name = "buttonRight";
-            this.buttonRight.Size = new System.Drawing.Size(50, 50);
-            this.buttonRight.TabIndex = 7;
-            this.buttonRight.Text = ">";
-            this.buttonRight.UseVisualStyleBackColor = true;
-            // 
-            // buttonLeft
-            // 
-            this.buttonLeft.Location = new System.Drawing.Point(11, 394);
-            this.buttonLeft.Name = "buttonLeft";
-            this.buttonLeft.Size = new System.Drawing.Size(50, 50);
-            this.buttonLeft.TabIndex = 8;
-            this.buttonLeft.Text = "<";
-            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.formSizeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.formSizeLabel.AutoSize = true;
+            this.formSizeLabel.Location = new System.Drawing.Point(20, 530);
+            this.formSizeLabel.Name = "formSizeLabel";
+            this.formSizeLabel.Size = new System.Drawing.Size(148, 20);
+            this.formSizeLabel.TabIndex = 2;
+            this.formSizeLabel.Text = "Form size: 800, 600";
             // 
             // mainForm
             // 
@@ -170,6 +178,8 @@
             this.Controls.Add(this.targetButton);
             this.Name = "mainForm";
             this.Text = "(0,0)  Winforms Components - Common Controls";
+            this.Move += new System.EventHandler(this.mainForm_Move);
+            this.Resize += new System.EventHandler(this.mainForm_Resize);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -184,7 +194,7 @@
         private System.Windows.Forms.Label formSizeLabel;
         private System.Windows.Forms.Button buttonLeft;
         private System.Windows.Forms.Button buttonRight;
-        private System.Windows.Forms.Button buttondown;
+        private System.Windows.Forms.Button buttonDown;
         private System.Windows.Forms.Button buttonUp;
         private System.Windows.Forms.Button buttonCenter;
         private System.Windows.Forms.Button buttonEnable;
--- WindowsFormsAppCommonProperties/mainForm.cs
+++ WindowsFormsAppCommonProperties/mainForm.cs
@@ -15,6 +15,162 @@ namespace WindowsFormsAppCommonProperties
         public mainForm()
         {
             InitializeComponent();
+            this.Size = new Size(800, 600);
+            localizarPosicionTituloForm();
+            localizarPosicionLabelForm();
+            iniciarListBox();
+            locationTargetButton();
+        }
+
+        private void localizarPosicionTituloForm()
+        {
+            string x = this.Location.X.ToString();
+            string y = this.Location.Y.ToString();
+
+            this.Text = "(" + x + ", " + y + ") Winforms Components - Common Controls";
+        }
+
+        private void localizarPosicionLabelForm()
+        {
+            string x = this.Size.Width.ToString();
+            string y = this.Size.Height.ToString();
+
+            formSizeLabel.Text = "Form size " + x + ", " + y;
+        }
+
+        private void mainForm_Move(object sender, EventArgs e)
+        {
+            localizarPosicionTituloForm();
+        }
+
+        private void mainForm_Resize(object sender, EventArgs e)
+        {
+            localizarPosicionLabelForm();
+        }
+
+        private void iniciarListBox()
+        {
+            comboBox1.Items.Add("None");
+            comboBox1.Items.Add("Fixed single");
+            comboBox1.Items.Add("Fixed 3D");
+            comboBox1.Items.Add("Fixed Dialog");
+            comboBox1.Items.Add("Sizable");
+            comboBox1.Items.Add("Fixed tool window");
+            comboBox1.Items.Add("Sizable tool window");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "None")
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            }
+            if (comboBox1.SelectedItem == "Fixed single")
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            }
+            if (comboBox1.SelectedItem == "Fixed 3D")
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            }
+            if (comboBox1.SelectedItem == "Fixed Dialog")
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            }
+            if (comboBox1.SelectedItem == "Sizable")
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            }
+            if (comboBox1.SelectedItem == "Fixed tool window")
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            }
+            if (comboBox1.SelectedItem == "Sizable tool window")
+            {
+                this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            }
+
+        }
+
+        private void buttonVisible_Click(object sender, EventArgs e)
+        {
+            if (targetButton.Visible == false)
+            {
+                targetButton.Visible = true;
+                buttonVisible.Text = "Visible = true";
+            }
+            else
+            {
+                targetButton.Visible = false;
+                buttonVisible.Text = "Visible = false";
+            }
+        }
+
+        private void buttonEnable_Click(object sender, EventArgs e)
+        {
+            if (targetButton.Enabled == false)
+            {
+                targetButton.Enabled = true;
+                buttonEnable.Text = "Enabled = true";
+            }
+            else
+            {
+                targetButton.Enabled = false;
+                buttonEnable.Text = "Enabled = false";
+            }
+        }
+
+        private void buttonCenter_Click(object sender, EventArgs e)
+        {
+            int x = this.Size.Width / 2 - 50;
+            int y = this.Size.Height / 2 - 50;
+
+            targetButton.Location = new Point(x, y);
+            locationTargetButton();
+        }
+
+        private void locationTargetButton()
+        {
+            string x = targetButton.Location.X.ToString();
+            string y = targetButton.Location.Y.ToString();
+
+            targetButton.Text = "Target" + Environment.NewLine + "X = " + x + Environment.NewLine +"Y = " + y;
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            int x = targetButton.Location.X;
+            int y = targetButton.Location.Y;
+
+            targetButton.Location = new Point(x, y - 5);
+            locationTargetButton();
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            int x = targetButton.Location.X;
+            int y = targetButton.Location.Y;
+
+            targetButton.Location = new Point(x, y + 5);
+            locationTargetButton();
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            int x = targetButton.Location.X;
+            int y = targetButton.Location.Y;
+
+            targetButton.Location = new Point(x + 5, y);
+            locationTargetButton();
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            int x = targetButton.Location.X;
+            int y = targetButton.Location.Y;
+
+            targetButton.Location = new Point(x - 5, y);
+            locationTargetButton();
         }
     }
 }
